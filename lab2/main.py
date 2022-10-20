f5d8f368b1af09cd459ef1750242227e8909dfa8
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-10-20 12:35:13 +0200

    dodano rozwiązanie zadania2

--- lab2/main.py
+++ lab2/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+'''Zadanie 1 (1.py):
+• Dla osób poniżej 4 roku życia wstęp jest bezpłatny.
+• Dla osób w wieku od 4 do 18 lat bilet kosztuje 10zł.
+• Dla osób powyżej 18 roku życia bilet kosztuje 20zł.
+Przykład: Wprowadź wiek klienta: 5
+Cena biletu: 10zł'''
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = (int)(input("Proszę podać wiek klienta: "))
+if a<4:
+    print("Wstęp jest bezpłatny")
+elif a<=18:
+    print("Cena biletu: 10zł")
+else:
+    print("Cena biletu kosztuje 20zł")
