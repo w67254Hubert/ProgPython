144a659cc52c53b36137062f6c7ad7ffabae159b
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-10-20 13:15:50 +0200

    dodano rozwiązanie po zajęciach lab 2 1-4

---
+++ lab2/zadanie2.py
@@ -0,0 +1,8 @@
+# Napisz skrypt, który sprawdzi czy litera wprowadzona przez użytkownika jest duża czy mała.
+litera=input("podaj litere ")
+if "a"<=litera>='z':
+    print("litera jest mała")
+elif "A" <= litera <= 'Z':
+    print("litera jest durza")
+else:
+    print("to nie litera")
