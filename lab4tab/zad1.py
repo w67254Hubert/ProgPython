68dacdb6817dd1b1abf5e1dcdaef7e666e61a92d
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-11-03 13:27:40 +0100

    koniec zaj

---
+++ lab4tab/zad1.py
@@ -0,0 +1,16 @@
+'''Zadanie 1. Utwórz listę lista_zakupow zawierającą nazwy co najmniej czterech produktów spożywczych.
+Następnie:
+• wyświetl pierwszy i drugi element z listy
+• zmień trzeci element listy na „mleko”
+• wyświetl przedostatni i ostatni element listy'''
+
+t=["jaja", "bółka","banan","chleb"]
+print(t[0], t[1],)
+print()
+
+t[2]="mleko"
+print(t)
+
+print(t[-1], t[-2])
+
+
---
+++ lab4tab/zad2.py
@@ -0,0 +1,19 @@
+'''Zadanie 2.
+• Utwórz listę zestaw_1 zawierającą liczby losowe z przedziału [1, 10]. Liczbę elementów listy podaje
+użytkownik. Wyświetl listę.
+• Utwórz drugą listę zestaw_2 zawierającą liczby losowe z przedziału [5, 15]. Liczbę elementów listy
+podaje użytkownik. Wyświetl listę.
+• Pobierz od użytkownika liczbę. Napisz instrukcję warunkową, która na podstawie wartości
+zapisanych w listach wyświetli jeden z poniższych komunikatów: „Liczba z zestawu 1”, „Liczba z
+zestawu 2” albo „Nie ma takiej liczby w obu zestawach”.
+• Utwórz listę zestaw_1_2 będącą złączeniem wartości z list zestaw_1 oraz zestaw_2. Posortuj i wyświetl
+listę'''
+import random
+
+x=random.randint(1,10)
+zestaw_1=[]
+u=int(input("jak długa lista"))
+
+zestaw_1
+for i in range(u):
+
