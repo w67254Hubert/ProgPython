7b926dab4f54e7e0ebfe232ef302f0c8438c0d89
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-11-03 12:23:13 +0100

    zadania 5-7

--- lab3/main.py
+++
@@ -1,1 +0,0 @@
-#czwiczemie while
---
+++ lab4/zad7.py
@@ -0,0 +1,7 @@
+'''Zadanie 7 (7.py) Za pomocą pętli for wypisz na ekranie ciągi liczb:
+• 1, 2, 3, ... , 99, 100
+• 100, 99, ... , 2, 1, 0
+• 7, 14, 21, ... , 70, 77
+• 20, 18, ... , 2, 0'''
+
+
