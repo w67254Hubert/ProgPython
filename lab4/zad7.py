d1a393f7167df00190fcc1cd4ca1bceb596d3a87
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-11-03 13:04:28 +0100

    zadania do

--- lab4/zad7.py
+++ lab4/zad7.py
@@ -4,4 +4,16 @@
 • 7, 14, 21, ... , 70, 77
 • 20, 18, ... , 2, 0'''
 
+for i in  range(1,101):
+    print(i, end='   ')
+print()
+for a in range(100,-1,-1):
+    print(a, end='   ')
+print(end='/n ')
+for b in range(7,78,7):
+    print(b, end='   ')
+print()
+for c in range(20,-1,-2):
+    print(c, end='   ')
+print()
 
