6fd1b1bc060cd1651da7ba8721c36d8e933c2c7a
w67254Hubert <w67254>
2022-11-17 13:26:15 +0100

    koniec lab

--- lab5słow/zad2.py
+++ lab5słow/zad2.py
@@ -18,14 +18,57 @@ klucz:wartość) do nowego słownika.
 sample_dict = {"name": "Kelly","surname": "Jones","age": 25,"salary": 8000,"city": "New york"}
 
 for k,v in sample_dict.items():
-    print(k,v,)
+    print(f'{k:<10} = {v:>10}')
 print()
 
-list=["name","age"]
-D = {}
+list=["nie ma mnie","age"]
 
-for i in list:
-    for x in sample_dict.keys():
-        if(i == x):
-            D[i] = sample_dict[x]
+#do podpunktu 2
+'''D = {}
+for k in list:
+    for k2 in sample_dict.keys():
+        if(k == k2):
+            D[k] = sample_dict[k2]
+            break
+print(D)'''
+
+'''D = {}
+for k in list:
+    if k in sample_dict.keys():
+        D[k] = sample_dict[k]
+
+print(D)'''
+D = {k:sample_dict[k] for k in list if k in sample_dict.keys()}
 print(D)
+print()
+
+#wersje do jednego podpunktu
+#• Usuń ze słownika wartości, których klucze występują w liście.
+
+# for k in list:
+#     if k in sample_dict.keys():
+#         sample_dict.pop(k)
+# print(sample_dict)
+
+for k in list:
+        sample_dict.pop(k,None)
+print(sample_dict)
+
+
+#• Sprawdź, czy wartość „Jones” występuje w słowniku.
+
+# if "Jones" in dict1.values():
+#     print('element znajduje sie w słowniku')
+# else:
+#     print('nie ma ')
+
+for k in sample_dict.values():
+    if k == "Jones":
+        print("istnieje")
+        break
+else:
+    print("nie istnieje")
+
+
+
+
