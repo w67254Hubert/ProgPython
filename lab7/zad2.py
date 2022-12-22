ce6779bbac1f3d7f434a3467b2da1650d0677b4d
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-12-22 13:19:05 +0100

    prawie koniec lab 7

--- lab7/zad2.py
+++ lab7/zad2.py
@@ -3,3 +3,16 @@
 metoda max(), min())
 • Wypisz największe elementy w każdym z wierszy (axis = 1) i w każdej z kolumn (axis = 0).
 • Policz sumę wartości w p  oszczególnych wierszach.'''
+import numpy as np
+
+macierz= np.random.randint(low=0 ,high=50 ,size=(5,5))
+print(macierz)
+
+print("wartość max" ,macierz.max())
+print("wartość min" ,macierz.min())
+
+print("wartość max w wierszu" ,macierz.max(axis=1))
+print("wartość max w kolumnie" ,macierz.max(axis=0))
+
+print("suma wartośi w wierszach", macierz.sum(axis=1))
+
