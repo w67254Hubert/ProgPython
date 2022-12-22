1df280a03f9402d04622b7cfb21015f755b02a74
w67254Hubert <w67254@student.wsiz.edu.pl>
2022-12-22 12:41:09 +0100

    laby7 2zad i dopkończenie 1zad

---
+++ lab7/zad2.py
@@ -0,0 +1,5 @@
+'''Zadanie 2.
+• Utwórz losową macierz o wymiarach 5x5. Znajdź największy i najmniejszy element. (patrz tab4_2d;
+metoda max(), min())
+• Wypisz największe elementy w każdym z wierszy (axis = 1) i w każdej z kolumn (axis = 0).
+• Policz sumę wartości w p  oszczególnych wierszach.'''
