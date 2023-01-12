1b8bd324bf11d1591c73229fcfbc7c2c2a95adcb
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-01-12 18:13:00 +0100

    Revert "tak"
    This reverts commit 6cbb16d49e5219a48499db162e59a2217434cc52.

--- lab8/zad1.py
+++
@@ -1,8 +0,0 @@
-'''Zadanie 1. Napisz funkcję find(), która w liście sprawdza czy występuje podana wartość. Lista i wartość są
-parametrami funkcji. Funkcja ma zwracać listę indeksów, pod którymi wystąpiła wartość. Nie wolno korzystać z
-operatora in w celu sprawdzenia czy wartość występuje w liście.'''
-
-
-def find(a,lista)
-    for x in range(len(lista)):
-    continue
