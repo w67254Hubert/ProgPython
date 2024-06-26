41a7e1c19268c8fa9e54bfdd691edc16941cd660
w67254Hubert <w67254@student.wsiz.edu.pl>
2024-06-26 13:43:54 +0200

    testy plików do kodu

--- testy.py
+++ testy.py
@@ -1,3 +1,4 @@
+# pip install 
 import pandas as pd
 
 dane=pd.read_csv('ścierzka\\do\\pliku.csv')#odczyt
@@ -99,9 +100,135 @@ lista.extend(krotka)
 #wyk 7 klasy
 #wyk 8 dziedziczenie
 import seaborn as sns
-import matplotlib.pyplot as plt
 #mam nadzieje ze będę mógł mieć wykresy podebrane z moodle wykl 9
 import os
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+#ez wykres liniowy bardzo podstawowy bez punktów 
+plt.plot([1, 2, 3, 4])
+plt.ylabel('jakieś liczby')
+plt.show()
+
+# wartości, najpierw dla wektora x, y
+# parametr w postaci stringa,określa styl wykresu
+#https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html#matplotlib.pyplot.plot
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro-')
+# lista parametrów w postaci [xmin, xmax, ymin, ymax]
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 5, 0.2)
+
+# wygenerować wiele wykresów na jednym "płótnie" (ang. canvas
+# podając niezbędne wartości: wartości dla osi x, wartości dla osi y, styl wykresu, ...
+plt.plot(x, x, 'r--', x, x**2, 'bs', x, x**4, 'g^')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 2, 100)
+# wykresy mogą być też dodawane do płótna definicja po definicji zamiast w pojedynczym wywołaniu funkcji plot()
+# tutaj też mam parametr label, który określa nazwę danego wykresu w legendzie
+plt.plot(x, x, label='liniowa')
+plt.plot(x, x**2, label='kwadratowa')
+plt.plot(x, x**3, label='sześcienna')
+# etykiety osi i nazwa
+plt.xlabel('etykieta x')
+plt.ylabel('etykieta y')
+plt.title("Prosty wykres")
+#pokarze legende
+plt.legend()
+plt.show()
+
+
+#to samo co wyrzej tylko przyjmujemy sinusa
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.arange(0, 10, 0.1)
+s = np.sin(x)
+plt.plot(x, s, label='sin(x)')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.title("Wykres sin(x)")
+plt.legend()
+plt.show()
+
+#scater plot z R
+import matplotlib.pyplot as plt
+import numpy as np
+# dane w formie słownika, ale równie dobrze może to być Pandas DataFrame
+data = {'a': np.arange(50),
+ 'c': np.random.randint(0, 50, 50),
+ 'd': np.random.randn(50)}
+
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+# aby w ten sposób przekazać parametry wykresu należy dodać niezbędny 
+# parametr data, który zawiera dane dostępne poprzez etykiety
+# to oznacza, że 'a' jest równoważne data['a'] itd. Parametr c to skrót od 
+# color, tutaj przekazywany w formie wektora
+# wartości kolorów dla każdej kolejnej wartości wykresu. Parametr s to 
+# scale - określa rozmiar, w tym przypadku punktu, dla
+# każdej kolejnej wartości wektora wykresu. Reasumując dla pierwszego 
+# punktu wykresu będą brane poniższe wartości
+print(f"a={data['a'][0]}, b={data['b'][0]}, c={data['c'][0]}, 
+d={data['d'][0]}")
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('wartość a')
+plt.ylabel('wartość b')
+plt.show()
+
+#potem robi wiele wykresów jako jeden wynik ale to chyba za dużo 
+
+#prosty wykres słupkowy
+import numpy as np
+import matplotlib.pyplot as plt
+etykiety = ['K', 'M']
+wartosci = [345, 435]
+plt.bar(etykiety, wartosci)
+plt.ylabel('Ilość narodzin')
+plt.xlabel('Płeć')
+plt.show()
+
+
+
+#wykres kołowy mało przydatny ale ładnie wygląda
+zawodnicy = ['Messi', 'Suarez', 'Dembele', 'Coutinho']
+bramki = [48, 25, 13, 11]
+def prepare_label(pct, br):
+ absolute = int(pct / 100. * np.sum(br))
+ return "{:.1f}%".format(pct, absolute)
+wedges, texts, autotexts = plt.pie(bramki, labels=zawodnicy,
+ autopct=lambda pct: prepare_label(pct, 
+bramki), textprops=dict(color="black"))
+plt.setp(autotexts, size=14, weight="bold")
+plt.legend(title='Zawodnicy')
+plt.show()
+
+#potem rob i zaawanwsowane tutorial pod linkiem
+# https://towardsdatascience.com/matplotlib-tutorial-learn-basics-of-pythons-powerful-plotting-library-b5d1b8f67596
+
+
+#rozkład prawdopodobieństwa
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.random.randn(10000)
+# bins oznacza ilość "koszy" czyli słupków, do których mają wpadać wartości z wektora x
+# facekolor oznacza kolor słupków
+# alpha to stopień przezroczystości wykresu
+# density oznacza czy suma ilości zostanie znormalizowana do rozkładu prawdopodobieństwa (czyli przedział 0, 1)
+plt.hist(x, bins=50, facecolor='g', alpha=0.75, density=True)
+plt.xlabel('Wartości')
+plt.ylabel('Prawdopodobieństwo')
+plt.title('Histogram')
+# wyświatlanie siatki
+plt.grid(True)
+plt.show()
